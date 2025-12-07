9de96fd71286681619b1de2b52af9f7410ffe116
Ian <ian.nasoore@student.moringaschool.com>
2025-12-07 08:19:57 +0300

    hez

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,9 +1,40 @@
 class Article:
+    all = []
+    
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
+        self._author = author
+        self._magazine = magazine
+        self._title = None
         self.title = title
-        
+        Article.all.append(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if not hasattr(self, '_title') or self._title is None:
+            if isinstance(value, str) and 5 <= len(value) <= 50:
+                self._title = value
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, value):
+        self._author = value
+    
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, value):
+        self._magazine = value
+
+
 class Author:
     def __init__(self, name):
         self.name = name
