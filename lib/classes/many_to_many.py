8e16a4060fb2e4d7271bc8a89216deb8acd8cf3a
Ian <ian.nasoore@student.moringaschool.com>
2025-12-07 08:39:00 +0300

    Auto: push from Copilot

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -37,33 +37,110 @@ class Article:
 
 class Author:
     def __init__(self, name):
+        self._name = None
         self.name = name
-
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not hasattr(self, '_name') or self._name is None:
+            if isinstance(value, str) and len(value) > 0:
+                self._name = value
+          
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        magazines = []
+        seen = set()  
+        for article in self.articles():
+            if article.magazine not in seen:
+                magazines.append(article.magazine)
+                seen.add(article.magazine)
+        return magazines
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self.articles():
+            return None
+        areas = []
+        seen = set() 
+        for article in self.articles():
+            category = article.magazine.category
+            if category not in seen:
+                areas.append(category)
+                seen.add(category)
+        return areas
+
+
+   
 
 class Magazine:
-    def __init__(self, name, category):
+      all = []
+    
+      def __init__(self, name, category):
+        self._name = None
+        self._category = None
         self.name = name
         self.category = category
+        Magazine.all.append(self)
+    
+      @property
+      def name(self):
+        return self._name
+    
+      @name.setter
+      def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+    
+      @property
+      def category(self):
+        return self._category
+    
+      @category.setter
+      def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
 
-    def articles(self):
-        pass
+      def articles(self):
+        return [article for article in Article.all if article.magazine == self]
 
-    def contributors(self):
-        pass
+      def contributors(self):
+        authors = []
+        seen = set()
+        for article in self.articles():
+            if article.author not in seen:
+                authors.append(article.author)
+                seen.add(article.author)
+        return authors
 
-    def article_titles(self):
-        pass
+      def article_titles(self):
+        titles = [article.title for article in self.articles()]
+        return titles if titles else None
 
-    def contributing_authors(self):
-        pass
+      def contributing_authors(self):
+        author_counts = {}
+        for article in self.articles():
+            author = article.author
+            author_counts[author] = author_counts.get(author, 0) + 1
+        
+        authors = [author for author, count in author_counts.items() if count > 2]
+        return authors if authors else None
+    
+      @classmethod
+      def top_publisher(cls):
+        if not Article.all:
+            return None
+        
+        magazine_counts = {}
+        for article in Article.all:
+            magazine = article.magazine
+            magazine_counts[magazine] = magazine_counts.get(magazine, 0) + 1
+        
+        return max(magazine_counts, key=magazine_counts.get)
